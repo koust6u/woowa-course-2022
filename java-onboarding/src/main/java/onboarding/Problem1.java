3f086d776396a60e997952b657944391a6447b6d
minjong <koust6u@pusan.ac.kr>
2023-09-25 21:29:13 +0900

    feat(problem1): pobi와 crong의 이 펼친 페이지가 유효한지 체크

--- java-onboarding/src/main/java/onboarding/Problem1.java
+++ java-onboarding/src/main/java/onboarding/Problem1.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!validation(pobi, crong)) return -1;
+        return 0;
     }
+
+    private static boolean validation(List<Integer> pobi, List<Integer> crong){
+        return pobi.size() == 2 && crong.size() == 2
+                && pobi.get(0) + 1 == pobi.get(1)
+                && crong.get(0) + 1 == crong.get(1)
+                && pobi.get(0) % 2 == 1
+                && crong.get(0) % 2 == 1
+                && pobi.get(0)> 0
+                && crong.get(0)> 0
+                && pobi.get(1) <= 400
+                && crong.get(1) <= 400;
+    }
+
 }
