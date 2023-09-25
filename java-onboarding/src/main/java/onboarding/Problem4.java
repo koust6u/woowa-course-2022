5f3d210a4329537444abb74088a7354c9f14f4ac
minjong <koust6u@pusan.ac.kr>
2023-09-25 23:03:34 +0900

    feat(problem4): 글자 수 이동을 로직 작성

--- java-onboarding/src/main/java/onboarding/Problem4.java
+++ java-onboarding/src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return shiftCipher(word);
+    }
+
+    private static String shiftCipher(String plaintext){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < plaintext.length(); i++){
+            if (plaintext.charAt(i) >= 'a' && plaintext.charAt(i) <= 'z')
+                sb.append((char)('z' -plaintext.charAt(i) + 'a'));
+            else if (plaintext.charAt(i) >= 'A' && plaintext.charAt(i) <= 'Z')
+                sb.append((char)('Z' - plaintext.charAt(i) + 'A'));
+            else sb.append(plaintext.charAt(i));
+        }
+        return sb.toString();
     }
 }
