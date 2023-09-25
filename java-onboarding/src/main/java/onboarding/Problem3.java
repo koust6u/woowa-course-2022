2ed788a58f1271726e9a2fced045d45fae594dd8
minjong <koust6u@pusan.ac.kr>
2023-09-25 22:34:36 +0900

    feat(problem3): 3번 문제 stream으로 작성

--- java-onboarding/src/main/java/onboarding/Problem3.java
+++ java-onboarding/src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Problem3 {
+
+    private static final List<Integer> list = List.of(3,6,9);
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return  (int)IntStream.rangeClosed(1, number)
+                .mapToObj(String::valueOf)
+                .flatMap(element -> Arrays.stream(element.split("")))
+                .mapToInt(Integer::parseInt)
+                .filter(list::contains)
+                .count();
     }
+
+
 }
