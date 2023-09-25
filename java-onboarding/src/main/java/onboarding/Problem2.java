f1d26d6f56cd4175f900e1074263c326a63f4f30
minjong <koust6u@pusan.ac.kr>
2023-09-25 21:39:02 +0900

    feat(problem2): 문자열에서 2번 연속되는 문자열 삭제 로직 작성

--- java-onboarding/src/main/java/onboarding/Problem2.java
+++ java-onboarding/src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static String compact(String cipher){
+        StringBuilder sb = new StringBuilder();
+        for (int i =0 ; i < cipher.length()-1; i++){
+            if (cipher.charAt(i) == cipher.charAt(i+1)) i++;
+            else sb.append(cipher.charAt(i));
+        }
+        return sb.toString();
     }
 }
